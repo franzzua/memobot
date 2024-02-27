97fe5fddb616393493a8f04e1a19b3fac43e0fdc
Andrei Khamidulin <fransua.ne@gmail.com>
2024-02-27 22:07:52 +0100

    rm webhook

--- src/bot/memobot.ts
+++ src/bot/memobot.ts
@@ -6,10 +6,7 @@ if (!process.env.BOT_TOKEN)
 if(!process.env.PUBLIC_URL)
     throw new Error(`WEBHOOK_ADDRESS is not defined`);
 
-export const bot = new Telegraf(process.env.BOT_TOKEN, {
-    telegram: { webhookReply: true },
-});
-bot.telegram.setWebhook(`${process.env.PUBLIC_URL}/api/telegraf/${bot.secretPathComponent()}`)
+export const bot = new Telegraf(process.env.BOT_TOKEN);
 // bot.command('oldschool', (ctx) => ctx.reply('Hello'))
 bot.command('menu', Telegraf.reply('λ'))
 // bot.start((ctx) => ctx.reply('Welcome'))
