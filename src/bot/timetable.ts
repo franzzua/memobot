cf8f9be52f1700c7f42cd81cdfebe1810252193a
roman-spix <sosnin.roman@gmail.com>
2024-05-21 16:47:58 +0200

    Update timetable.ts
    Updated test time intervals

--- src/bot/timetable.ts
+++ src/bot/timetable.ts
@@ -1,10 +1,11 @@
 export const Timetable = [
-    { index: 0, time: 4, name: '4s'},
-    { index: 1, time: 11, name: '11s'},
-    { index: 2, time: 29, name: '29s'},
-    { index: 3, time: 1*60, name: '1m'},
-    { index: 4, time: 3*60, name: '3m'},
-    { index: 5, time: 7*60, name: '7m'},
+    { index: 0, time: 3*60, name: '3 mins'},
+    { index: 1, time: 112*60, name: '1 hr 50 mins'},
+    { index: 2, time: 196*60, name: '3 hrs 20 mins'},
+    { index: 3, time: 784*60, name: '13 hrs'},
+    { index: 4, time: 1568*60, name: '1 d 2 hrs'},
+    { index: 5, time: 3360*60, name: '2 d 8 hrs'},
+    { index: 6, time: 10080*60, name: '1 w'},
 ]
 
 const min = 60;
@@ -14,11 +15,11 @@ const week = 7 * day;
 const month = 30 * day;
 
 export const TimetableProd = [
-    { index: 0, time: 42*min, name: '42 mins'},
-    { index: 1, time: 24*hour, name: '24 hrs'},
-    { index: 2, time: 42*hour, name: '42 hrs'},
-    { index: 3, time: week, name: '1 w'},
-    { index: 4, time: 2*week, name: '2 w'},
-    { index: 5, time: month, name: '1 m'},
-    { index: 5, time: 2*month, name: '2 m'},
+    { index: 0, time: 3*min, name: '3 mins'},
+    { index: 1, time: 112*min, name: '1 hr 50 mins'},
+    { index: 2, time: 196*min, name: '3 hrs 20 mins'},
+    { index: 3, time: 784*min, name: '13 hrs'},
+    { index: 4, time: 1568*min, name: '1 d 2 hrs'},
+    { index: 5, time: 3360*min, name: '2 d 8 hrs'},
+    { index: 6, time: 10080*min, name: '1 w'},
 ]
