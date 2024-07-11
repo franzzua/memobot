5f17df31ca342ed6d779228ba63325ed0a6ac429
roman-spix <sosnin.roman@gmail.com>
2024-07-11 12:56:24 +0400

    Update donate.ts
    Command texts

--- src/api/donate.ts
+++ src/api/donate.ts
@@ -2,7 +2,7 @@ import { CommandContext } from "./types";
 import { Context } from "telegraf";
 
 export async function onDonate(ctx: CommandContext){
-    return ctx.reply('💳 Choose your payment method', {
+    return ctx.reply('💳 Please select your payment method', {
         reply_markup: {
             inline_keyboard: [
                 [
@@ -29,14 +29,14 @@ export async function onCallback(ctx: Context){
     })
 }
 export const payments: Record<PaymentType, string> = {
-    USD: `💵 Click to proceed with <a href="https://donate.stripe.com/14k2c4abp1Fhf2o8wy">Stripe</a>`,
-    EUR: `💶 Click to proceed with <a href="https://revolut.me/newtimesroman">Revolut</a>`,
-    PayPal: `🌐 Click to proceed with <a href="https://paypal.me/spixenglish">PayPal</a>`,
-    RUB: `🏦 Click to proceed with <a href="https://pay.cloudtips.ru/p/aa517638">Tinkoff</a>`,
-    USDT: `🔗 Copy to proceed with USDT: <code>0x5d00b2104332feef90cfe4b7eb8be5f7b224ff7a</code>`,
-    BTC: `🔗 Copy to proceed with BTC: <code>1PnaC2R3XC7yuogz6vJe4NbPGcK9Jy8P2X</code>`,
-    ETH: `🔗 Copy to proceed with ETH: <code>0x5d00b2104332feef90cfe4b7eb8be5f7b224ff7a</code>`,
-    TON: `🔗 Copy to proceed with TON: <code>UQBwCIZdi8uuWm-gwS_8pTnaMWFYIrkiUpodeWlVsDJbPHPJ</code>`,
+    USD: `💵 Pay with <a href="https://donate.stripe.com/14k2c4abp1Fhf2o8wy">Stripe</a>`,
+    EUR: `💶 Pay with <a href="https://revolut.me/newtimesroman">Revolut</a>`,
+    PayPal: `🌐 Pay with <a href="https://paypal.me/spixenglish">PayPal</a>`,
+    RUB: `🏦 Pay with <a href="https://pay.cloudtips.ru/p/aa517638">Tinkoff</a>`,
+    USDT: `🔗 Pay with USDT: <code>0x5d00b2104332feef90cfe4b7eb8be5f7b224ff7a</code>`,
+    BTC: `🔗 Pay with Bitcoin (BTC): <code>1PnaC2R3XC7yuogz6vJe4NbPGcK9Jy8P2X</code>`,
+    ETH: `🔗 Pay with Ethereum (ETH): <code>0x5d00b2104332feef90cfe4b7eb8be5f7b224ff7a</code>`,
+    TON: `🔗 Pay with TON: <code>UQBwCIZdi8uuWm-gwS_8pTnaMWFYIrkiUpodeWlVsDJbPHPJ</code>`,
 }
 
 export type PaymentType =
