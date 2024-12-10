d10dbc21fe05b500fadb9e2b11289593e5f0871a
fransua <fransua.ne@gmail.com>
2024-12-10 22:56:34 +0100

    f

--- .github/auth-init.sh
+++ .github/auth-init.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
 export project='steel-topic-444112-i8'
+export repo_owner='franzzua'
+export repo_name='memobot'
+export deployer_name=github
+export runner_name=app-runner
+
+export deployer=${deployer_name}@${project}.iam.gserviceaccount.com
+export runner=${runner_name}@${project}.iam.gserviceaccount.com
+
 gcloud config set project ${project}
 
-gcloud iam service-accounts create "github" --project "${project}"
+gcloud iam service-accounts create "${deployer_name}" --project "${project}"
+gcloud iam service-accounts create "${runner_name}" --project "${project}"
 
 gcloud projects add-iam-policy-binding ${project} \
   --role="roles/cloudfunctions.admin" \
-  --member="serviceAccount:github@${project}.iam.gserviceaccount.com"
+  --member="serviceAccount:${deployer}"
 
 gcloud projects add-iam-policy-binding ${project} \
   --role="roles/iam.serviceAccountTokenCreator" \
-  --member="serviceAccount:github@${project}.iam.gserviceaccount.com"
+  --member="serviceAccount:${deployer}"
 
 gcloud projects add-iam-policy-binding ${project} \
   --role="roles/iam.serviceAccountUser" \
-  --member="serviceAccount:github@${project}.iam.gserviceaccount.com"
+  --member="serviceAccount:${deployer}"
 
 gcloud iam workload-identity-pools create "github" --project="${project}" --location="global" --display-name="GitHub Actions Pool"
-gcloud iam workload-identity-pools describe "github" --project="${project}" --location="global" --format="value(name)"
+export pool=$(gcloud iam workload-identity-pools describe "github" --project="${project}" --location="global" --format="value(name)")
 
 gcloud iam workload-identity-pools providers create-oidc "github" \
   --project="${project}"  \
@@ -26,7 +35,7 @@ gcloud iam workload-identity-pools providers create-oidc "github" \
   --workload-identity-pool="github"  \
   --display-name="My GitHub repo Provider"  \
   --attribute-mapping="google.subject=assertion.sub,attribute.actor=assertion.actor,attribute.repository=assertion.repository,attribute.repository_owner=assertion.repository_owner"  \
-  --attribute-condition="assertion.repository_owner == 'franzzua'"  \
+  --attribute-condition="assertion.repository_owner == '${repo_owner}'"  \
   --issuer-uri="https://token.actions.githubusercontent.com"
 
 
@@ -36,33 +45,32 @@ gcloud iam workload-identity-pools providers describe "github" \
   --workload-identity-pool="github" \
   --format="value(name)"
 
-gcloud iam service-accounts add-iam-policy-binding "github@${project}.iam.gserviceaccount.com" \
+gcloud iam service-accounts add-iam-policy-binding "${deployer}" \
   --project="${project}" \
   --role="roles/iam.workloadIdentityUser" \
-  --member="principalSet://iam.googleapis.com/projects/367509383184/locations/global/workloadIdentityPools/github/attribute.repository/AwtorGit/monorepo"
+  --member="principalSet://iam.googleapis.com/${pool}/attribute.repository/${repo_owner}/${repo_name}"
 
 gcloud projects add-iam-policy-binding ${project} \
   --role="roles/artifactregistry.repoAdmin" \
-  --member="serviceAccount:github@${project}.iam.gserviceaccount.com"
+  --member="serviceAccount:${deployer}"
 
 gcloud projects add-iam-policy-binding ${project} \
   --role="roles/iam.serviceAccountTokenCreator" \
-  --member="serviceAccount:github@${project}.iam.gserviceaccount.com"
+  --member="serviceAccount:${deployer}"
 
 gcloud projects add-iam-policy-binding ${project} \
   --role="roles/run.admin" \
-  --member="serviceAccount:github@${project}.iam.gserviceaccount.com"
+  --member="serviceAccount:${deployer}"
 
 gcloud projects add-iam-policy-binding ${project} \
   --role="roles/iam.serviceAccountUser" \
-  --member="serviceAccount:github@${project}.iam.gserviceaccount.com"
+  --member="serviceAccount:${deployer}"
 
 gcloud projects add-iam-policy-binding ${project} \
   --role="roles/storage.admin" \
-  --member="serviceAccount:github@${project}.iam.gserviceaccount.com"
+  --member="serviceAccount:${deployer}"
 
 
-gcloud iam service-accounts create "app-runner" --project "${project}"
 
 gcloud projects add-iam-policy-binding ${project} \
   --role="roles/aiplatform.serviceAgent" \
@@ -102,4 +110,5 @@ gcloud services enable iamcredentials.googleapis.com
 gcloud services enable secretmanager.googleapis.com
 gcloud services enable artifactregistry.googleapis.com
 gcloud services enable run.googleapis.com
+gcloud services enable cloudfunctions.googleapis.com
 gcloud services enable translate.googleapis.com
