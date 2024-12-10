13f3071e58a363161468bfc1eab711a934e57c1e
fransua <fransua.ne@gmail.com>
2024-12-10 22:58:00 +0100

    f

--- .github/auth-init.sh
+++ .github/auth-init.sh
@@ -111,4 +111,5 @@ gcloud services enable secretmanager.googleapis.com
 gcloud services enable artifactregistry.googleapis.com
 gcloud services enable run.googleapis.com
 gcloud services enable cloudfunctions.googleapis.com
+gcloud services enable cloudbuild.googleapis.com
 gcloud services enable translate.googleapis.com
