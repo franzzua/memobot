7f31c503e96c68dfa5ea57dfcb7ff610629ffdf4
Andrei Khamidulin <fransua.ne@gmail.com>
2025-05-12 16:17:43 +0200

    log init error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { di } from "@di";
 import { Logger } from "./logger/logger";
 import { GCSLogger } from "./logger/gcs.logger";
+import {telegram} from "./functions";
 export { telegram, task } from "./functions";
 
 di.override(Logger, GCSLogger);
+telegram(null as any, null as any);
