ff8bb57a0605a79b45bcfa26cae54cff784fc089
Andrei Khamidulin <fransua.ne@gmail.com>
2025-05-12 15:10:05 +0200

    fix

--- src/functions/telegram.ts
+++ src/functions/telegram.ts
@@ -3,15 +3,19 @@ import {TelegrafApi} from "../api/telegraf.api";
 import {baseFunction} from "./base";
 import {Messenger} from "../messengers/messenger";
 import {getMessenger} from "./getMessenger";
-import {Logger} from "../logger/logger";
 
-const context = di.child();
-context.factory(Messenger, c => getMessenger('telegram', c));
-const tg = context.resolve(TelegrafApi);
-console.log(context.resolve(Logger).constructor.name, di.resolve(Logger).constructor.name);
-tg.init();
+let tg: TelegrafApi;
+export async function initTelegram() {
+    const context = di.child();
+    context.factory(Messenger, c => getMessenger('telegram', c));
+    tg = context.resolve(TelegrafApi);
+    await tg.init();
+}
 
-export const telegram = baseFunction('telegram', async (req, res) => {
-    await tg.messenger.handle(req, res);
-});
+export const telegram = Object.assign(
+    baseFunction('telegram', async (req, res) => {
+        await tg.messenger.handle(req, res);
+    }), {
+        init: initTelegram
+    });
 
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { di } from "@di";
 import { Logger } from "./logger/logger";
 import { GCSLogger } from "./logger/gcs.logger";
+import { telegram, task } from "./functions";
 
 di.override(Logger, GCSLogger);
-// is used by Google Function
-const { telegram, task } = await import("./functions");
 
-export { telegram, task };
+export default {
+    get telegram() {
+        telegram.init();
+        return telegram;
+    },
+    task
+}
