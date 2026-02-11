12f42fb1e52974dccaa0b3a22c28dbcd7ee3a795
Andrei Khamidulin <fransua.ne@gmail.com>
2026-02-11 20:36:45 +0100

    log init time

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,13 @@ import {GCSLogger} from "./logger/gcs.logger";
 import {execSync} from "node:child_process";
 import path from "node:path";
 
-execSync(`npx prisma generate`, {
-    stdio: 'inherit',
-    cwd: path.dirname(import.meta.dirname),
-    env: process.env,
-});
-
-console.log('migrate to ', process.env.DATABASE_URL?.replace(/:\/\/.*\@/, '://***@'));
-
-execSync(`npx prisma migrate deploy`, {
-    stdio: 'inherit',
-    cwd: path.dirname(import.meta.dirname),
-    env: process.env,
-});
-
 di.override(Logger, GCSLogger);
+di.resolve(Logger).measure(() => execSync(`npx prisma generate && npx prisma migrate deploy`, {
+    stdio: 'inherit',
+    cwd: path.dirname(import.meta.dirname),
+    env: process.env,
+}), 'prisma init ' + process.env.DATABASE_URL?.replace(/:\/\/.*\@/, '://***@'))
+
 
 const telegram = await import("./functions/telegram");
 export {telegram};
