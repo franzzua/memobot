ce1f40d0dad46d092808a010fcba4f0c0c1a5c05
Andrei Khamidulin <fransua.ne@gmail.com>
2026-02-11 19:38:24 +0100

    log

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import {Logger} from "./logger/logger";
 import {GCSLogger} from "./logger/gcs.logger";
 import {execSync} from "node:child_process";
 
-console.log('migrate to ', process.env.DATABASE_URL?.substring(-3));
+console.log('migrate to ', process.env.DATABASE_URL?.replace(/:\/\/.*\@/, '://***@'));
 
 execSync(`npx prisma migrate deploy`, {
     stdio: 'inherit',
