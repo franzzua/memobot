2dbfc6ab19d9be99ea7f09965c1acc1f91c8dde3
Andrei Khamidulin <fransua.ne@gmail.com>
2026-02-11 20:33:03 +0100

    ignore client

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,23 @@ import {di} from "@cmmn/core";
 import {Logger} from "./logger/logger";
 import {GCSLogger} from "./logger/gcs.logger";
 import {execSync} from "node:child_process";
+import path from "node:path";
+
+execSync(`npx prisma generate`, {
+    stdio: 'inherit',
+    cwd: path.dirname(import.meta.dirname),
+    env: process.env,
+});
 
 console.log('migrate to ', process.env.DATABASE_URL?.replace(/:\/\/.*\@/, '://***@'));
 
 execSync(`npx prisma migrate deploy`, {
     stdio: 'inherit',
+    cwd: path.dirname(import.meta.dirname),
     env: process.env,
 });
 
 di.override(Logger, GCSLogger);
 
-export {telegram} from "./functions/telegram";
+const telegram = await import("./functions/telegram");
+export {telegram};
