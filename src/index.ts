678913f15c295ea36913232723942bcd8003d7a6
Andrei Khamidulin <fransua.ne@gmail.com>
2026-02-12 19:05:54 +0100

    f

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import {di} from "@cmmn/core";
 import {Logger} from "./logger/logger";
 import {GCSLogger} from "./logger/gcs.logger";
+import {execSync} from "node:child_process";
+import path from "node:path";
 
 di.override(Logger, GCSLogger);
+const logger = di.resolve(Logger);
+logger.send({ event: 'init', db: process.env.DATABASE_URL?.replace(/:\/\/.*\@/, '://***@')});
+logger.measure(() => execSync(`npx prisma migrate deploy`, {
+    stdio: 'inherit',
+    cwd: path.dirname(import.meta.dirname),
+    env: process.env,
+}), 'prisma migrate')
+
+
 const telegram = await import("./functions/telegram");
 export {telegram};
