f737d672a3a0451c068e0b573b0943c1648c7d4b
Andrei Khamidulin <fransua.ne@gmail.com>
2026-02-12 19:59:20 +0100

    f

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,10 +1,9 @@
-import {PrismaClient} from "./client/client";
+import {PrismaClient} from "./client/client.js";
 import {Pool} from 'pg'
 import {PrismaPg} from '@prisma/adapter-pg'
 
-export function prismaFactory() {
+export function prismaFactory(): PrismaClient {
     const pool = new Pool({connectionString: process.env.DATABASE_URL})
     const adapter = new PrismaPg(pool)
-    const prisma = new PrismaClient({adapter});
-    return prisma;
+    return new PrismaClient({adapter});
 }
