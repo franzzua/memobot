05c1be2ff66242d950841309241ae58feed3133d
Andrei Khamidulin <fransua.ne@gmail.com>
2026-02-12 18:57:04 +0100

    add gcp-build step

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,7 @@
 import {di} from "@cmmn/core";
 import {Logger} from "./logger/logger";
 import {GCSLogger} from "./logger/gcs.logger";
-import {execSync} from "node:child_process";
-import path from "node:path";
 
 di.override(Logger, GCSLogger);
-di.resolve(Logger).measure(() => execSync(`npx prisma generate && npx prisma migrate deploy`, {
-    stdio: 'inherit',
-    cwd: path.dirname(import.meta.dirname),
-    env: process.env,
-}), 'prisma init ' + process.env.DATABASE_URL?.replace(/:\/\/.*\@/, '://***@'))
-
-
 const telegram = await import("./functions/telegram");
 export {telegram};
